cdbbf38e0422c295b1bb0be72a05f34f171212a5
jadson179 <jadson44.santos@gmail.com>
2020-03-28 12:00:33 -0300

    initial commit

--- build/precache-manifest.6a48cf580ed85b99ba86921d2273b9c7.js
+++ build/precache-manifest.6a48cf580ed85b99ba86921d2273b9c7.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "cb69d58391f18df0ab228bfd308dee16",
-    "url": "/index.html"
+    "url": "https://jadson179.github.io/codiv/build/index.html"
   },
   {
     "revision": "4f6fb66a21ef33eed74b",
-    "url": "/static/css/main.d6788ff5.chunk.css"
+    "url": "https://jadson179.github.io/codiv/build/static/css/main.d6788ff5.chunk.css"
   },
   {
     "revision": "0388a831b5a2f5b0ef6c",
-    "url": "/static/js/2.4c61a1a6.chunk.js"
+    "url": "https://jadson179.github.io/codiv/build/static/js/2.4c61a1a6.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.4c61a1a6.chunk.js.LICENSE.txt"
+    "url": "https://jadson179.github.io/codiv/build/static/js/2.4c61a1a6.chunk.js.LICENSE.txt"
   },
   {
     "revision": "4f6fb66a21ef33eed74b",
-    "url": "/static/js/main.f2164e0b.chunk.js"
+    "url": "https://jadson179.github.io/codiv/build/static/js/main.f2164e0b.chunk.js"
   },
   {
     "revision": "1b43b57a65a44c839edb",
-    "url": "/static/js/runtime-main.3a8fcfcd.js"
+    "url": "https://jadson179.github.io/codiv/build/static/js/runtime-main.3a8fcfcd.js"
   },
   {
     "revision": "3ee1f405b20fe060483caeb42f2a0a42",
-    "url": "/static/media/github.3ee1f405.svg"
+    "url": "https://jadson179.github.io/codiv/build/static/media/github.3ee1f405.svg"
   },
   {
     "revision": "2f4f94c753215ccbd1200972c17a1fde",
-    "url": "/static/media/logo.2f4f94c7.svg"
+    "url": "https://jadson179.github.io/codiv/build/static/media/logo.2f4f94c7.svg"
   }
 ]);
